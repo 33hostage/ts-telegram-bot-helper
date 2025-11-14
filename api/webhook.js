76ea203f6e6e6268e4e12414b816a46417b655ce
33hostage <142483710+33hostage@users.noreply.github.com>
2025-11-14 19:45:10 +0300

    feat: Initial setup for Telegram bot helper

---
+++ api/webhook.js
@@ -0,0 +1,28 @@
+import { Telegraf } from "telegraf";
+import * as dotenv from "dotenv";
+import { bot } from '../dist/index.js';
+dotenv.config();
+const WEBHOOK_SECRET = process.env.WEBHOOK_SECRET || process.env.BOT_TOKEN;
+const WEBHOOK_URL = process.env.WEBHOOK_URL;
+if (!WEBHOOK_URL || !WEBHOOK_SECRET) {
+    console.error("WEBHOOK_URL или BOT_TOKEN не найдены.");
+}
+export default async (req, res) => {
+    // Обработка входящего post запроса от telegram
+    if (req.method === 'POST' && req.body) {
+        try {
+            await bot.handleUpdate(req.body);
+            res.status(200).send('OK');
+        }
+        catch (e) {
+            console.error('Ошибка в Serverless-функции:', e);
+            res.status(200).send('Error processed');
+        }
+    }
+    else if (req.method === 'GET') {
+        res.status(200).send('Бот запущен и ожидает запросы');
+    }
+    else {
+        res.status(405).send('Метод не разрешен');
+    }
+};
